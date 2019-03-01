dec0b8d68e9cb8c9c7fc53b5ec2dc8a243262045
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2019-03-01 20:58:05 +0100

    Parser startup

--- src/Annotation/Parser/Parser.php
+++ src/Annotation/Parser/Parser.php
@@ -2,7 +2,63 @@
 
 namespace Igni\OpenApi\Annotation\Parser;
 
+use PhpParser\Error;
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\ParserFactory;
+
 class Parser
 {
+    private $fileImports;
+    private $phpParser;
 
+    public function __construct()
+    {
+        $this->phpParser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+    }
+
+    public function parse(string $docBlock): void
+    {
+        $tokenizer = new Tokenizer($docBlock);
+        $tokenizer->tokenize();
+    }
+
+    private function getFileImports(Context $context) : array
+    {
+        $filename = $context->getFilename();
+        if (isset($this->fileImports[$filename])) {
+            return $this->fileImports[$filename];
+        }
+
+        if (empty($filename) || !is_file($filename) || !is_readable($filename)) {
+            return $this->fileImports[$filename] = [];
+        }
+
+        $useStatements = [];
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new class($useStatements) extends NodeVisitorAbstract {
+            private $useStatements;
+
+            public function __construct(&$useStatements)
+            {
+                $this->useStatements = &$useStatements;
+            }
+
+            public function enterNode(Node $node) {
+                if ($node instanceof Node\Stmt\UseUse) {
+                    $this->useStatements[strtolower((string) $node->getAlias())] = (string) $node->name;
+                }
+            }
+        });
+
+        try {
+            $ast = $this->phpParser->parse(file_get_contents($filename));
+            $traverser->traverse($ast);
+        } catch (Error $exception) {
+            return $this->fileImports[$filename] = [];
+        }
+
+        return $this->fileImports[$filename] = $useStatements;
+    }
 }
